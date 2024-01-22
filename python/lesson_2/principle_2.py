ba2ee7b4b31b2890102756a62c039f30fe1a911b
Michael Santos <msantos@nyscf.org>
2024-01-22 12:53:24 -0500

    QUESTION, STUDENT_SOLUTION consts

--- python/lesson_2/principle_2.py
+++ python/lesson_2/principle_2.py
@@ -53,29 +53,37 @@ def tactic_1b():
     """
     print_completion(prompt)
 
-def tactic_2a():
-    """Instruct the model to work out its own solution before rushing to a conclusion"""
-    prompt = """
-        Determine if the student's solution is correct or not.
-
-        Question:
-        I'm building a solar power installation and I need \\
-        help working out the financials. 
+QUESTION = """
+        I'm building a solar power installation and I need help \\
+        working out the financials. 
         - Land costs $100 / square foot
         - I can buy solar panels for $250 / square foot
         - I negotiated a contract for maintenance that will cost \\
         me a flat $100k per year, and an additional $10 / square \\
         foot
-        What is the total cost for the first year of operations 
-        as a function of the number of square feet.
+        What is the total cost for the first year of operations \\
+        as a function of the number of square feet."""
 
-        Student's Solution:
+STUDENT_SOLUTION = """
         Let x be the size of the installation in square feet.
         Costs:
         1. Land cost: 100x
         2. Solar panel cost: 250x
         3. Maintenance cost: 100,000 + 100x
-        Total cost: 100x + 250x + 100,000 + 100x = 450x + 100,000
+        Total cost: 100x + 250x + 100,000 + 100x = 450x + 100,000"""
+
+def tactic_2a():
+    """Incorrect answer"""
+    prompt = f"""
+        Determine if the student's solution is correct or not.
+
+        Question:
+        {QUESTION}
+
+        Student's Solution:
+        {STUDENT_SOLUTION}
+        """
+    print_completion(prompt)
         """
     print_completion(prompt)
 
