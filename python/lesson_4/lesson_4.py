8089173647cd0fbe8c4d504501b043ca625e0f55
Michael Santos <msantos@nyscf.org>
2024-01-22 14:42:42 -0500

    ++ lesson_4.py, ++summarize_limit()

---
+++ python/lesson_4/lesson_4.py
@@ -0,0 +1,29 @@
+"""https://learn.deeplearning.ai/chatgpt-prompt-eng/lesson/4/summarizing"""
+from openai_wrapper import print_completion
+
+PRODUCT_REVIEW = """
+Got this panda plush toy for my daughter's birthday, \\
+who loves it and takes it everywhere. It's soft and \\
+super cute, and its face has a friendly look. It's \\
+a bit small for what I paid though. I think there \\
+might be other options that are bigger for the \\
+same price. It arrived a day earlier than expected, \\
+so I got to play with it myself before I gave it \\
+to her.
+"""
+
+def summarize_limit():
+    """Summarize with a word/sentence/character limit"""
+    prompt = f"""
+        Your task is to generate a short summary of a product review from an ecommerce site. 
+
+        Summarize the review below, delimited by triple backticks, in at most 30 words. 
+
+        Review:
+        ```
+        {PRODUCT_REVIEW}
+        ```
+    """
+    print_completion(prompt)
+
+summarize_limit()
