62e53daa30778b277eb575f5c727cfeb4eb591f1
Michael Santos <msantos@nyscf.org>
2024-01-25 15:26:41 -0500

    ++ summarize_price_and_value()

--- python/lesson_4/lesson_4.py
+++ python/lesson_4/lesson_4.py
@@ -31,5 +31,19 @@ def summarize_shipping_and_delivery():
     """
     print_completion(prompt)
 
+def summarize_price_and_value():
+    """Summarize with a focus on price and value"""
+    prompt = f"""
+        Your task is to generate a short summary of a product review from an ecommerce site to give feedback to the pricing deparmtment,
+        responsible for determining the price of the product.  
+
+        Summarize the review below, delimited by triple backticks, in at most 30 words, 
+        and focusing on any aspects that are relevant to the price and perceived value. 
+
+        {PRODUCT_REVIEW}
+    """
+    print_completion(prompt)
+
 summarize_word_sentence_character_limit()
 summarize_shipping_and_delivery()
+summarize_price_and_value()
