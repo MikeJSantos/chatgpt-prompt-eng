5b56d0b64bef20c922ee23da4554fbf0e5d3f92e
Michael Santos <msantos@nyscf.org>
2024-01-26 13:15:47 -0500

    ++ sentiment_one_word()
    - add wrapper to LAMP_REVIEW

--- python/lesson_5/lesson_5.py
+++ python/lesson_5/lesson_5.py
@@ -2,10 +2,13 @@
 from openai_wrapper import print_completion
 
 LAMP_REVIEW = """
+Review text:
+'''
 Needed a nice lamp for my bedroom, and this one had additional storage and not too high of a price point. Got it fast.
 The string to our lamp broke during the transit and the company happily sent over a new one. Came within a few days as well.
 It was easy to put together.  I had a missing part, so I contacted their support and they very quickly got me the missing piece!
 Lumina seems to me to be a great company that cares about their customers and products!!
+```
 """
 
 def sentiment():
@@ -14,11 +17,20 @@ def sentiment():
         What is the sentiment of the following product review, 
         which is delimited with triple backticks?
 
-        Review text:
-        '''
         {LAMP_REVIEW}
-        '''
     """
     print_completion(prompt)
 
-sentiment()
+def sentiment_one_word():
+    """Sentiment: one word answer"""
+    prompt = f"""
+        What is the sentiment of the following product review, which is delimited with triple backticks?
+
+        Give your answer as a single word, either "positive" or "negative".
+
+        {LAMP_REVIEW}
+    """
+    print_completion(prompt)
+
+# sentiment()
+sentiment_one_word()
