b24fa35694e5f9f228cb8fd46b4d45edf692d6ec
Michael Santos <msantos@nyscf.org>
2024-01-26 15:52:39 -0500

    ++ infer_5_topics()

--- python/lesson_5/lesson_5.py
+++ python/lesson_5/lesson_5.py
@@ -1,5 +1,5 @@
 """https://learn.deeplearning.ai/chatgpt-prompt-eng/lesson/5/inferring"""
-from openai_wrapper import print_completion
+from openai_wrapper import print_completion, get_completion
 
 LAMP_REVIEW = """
 Review text:
@@ -80,8 +80,48 @@ def identify_multiple_items():
     """
     print_completion(prompt)
 
+STORY = """
+Text sample:
+'''
+In a recent survey conducted by the government, public sector employees were asked to rate their level 
+of satisfaction with the department they work at. The results revealed that NASA was the most popular 
+department with a satisfaction rating of 95%.
+
+One NASA employee, John Smith, commented on the findings, stating, "I'm not surprised that NASA came out on top. 
+It's a great place to work with amazing people and incredible opportunities. I'm proud to be a part of 
+such an innovative organization."
+
+The results were also welcomed by NASA's management team, with Director Tom Johnson stating, "We are thrilled to 
+hear that our employees are satisfied with their work at NASA. We have a talented and dedicated team who work tirelessly 
+to achieve our goals, and it's fantastic to see that their hard work is paying off."
+
+The survey also revealed that the Social Security Administration had the lowest satisfaction rating, with 
+only 45% of employees indicating they were satisfied with their job. The government has pledged to address 
+the concerns raised by employees in the survey and work towards improving job satisfaction across all departments.
+'''
+"""
+
+def infer_5_topics():
+    """Inferring topics"""
+    prompt = f"""
+        Determine five topics that are being discussed in the following text, which is delimited by triple backticks.
+        Make each item one or two words long. 
+        Format your response as a list of items separated by commas.
+
+        {STORY}
+    """
+    response = get_completion(prompt)
+    # print(response)
+
+    topic_list = response.splitlines()
+    for i, topic in enumerate(topic_list):
+        topic_list[i] = topic[3:]
+
+    print(', '.join(topic_list))
+
 # sentiment()
 # sentiment_one_word()
 # identify_emotions()
 # identify_item_and_company()
-identify_multiple_items()
+# identify_multiple_items()
+infer_5_topics()
